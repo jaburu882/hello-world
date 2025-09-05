324d9bed55451d5709c22c5144b409928f225a4d
jaburu882 <marcus882@gmail.com>
2025-09-05 16:39:58 -0300

    feat: Adiciona o desafio do classificador de nível de herói

---
+++ desafio_nivel_heroi.js
@@ -0,0 +1,28 @@
+// armazena nome e nível do herói
+let Heroi = "Venom";
+let xp = 5500;
+
+// armazena nível do herói
+let nivel;
+
+// classificação com base no nível do herói
+if (xp < 1000) {
+  nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+  nivelDoHeroi = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+  nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+// saída 
+console.log(`O Herói de nome **${Heroi}** está no nível de **${nivel}**`);
